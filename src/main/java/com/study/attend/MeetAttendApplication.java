f814968af6995ca76ab3fb131e70074e78665a09
ksh <ksh164616@gmail.com>
2026-01-21 08:58:47 +0900

    빌드설정파일추가

---
+++ src/main/java/com/study/attend/MeetAttendApplication.java
@@ -0,0 +1,13 @@
+package com.study.attend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetAttendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetAttendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/attend/MeetAttendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.attend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetAttendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
